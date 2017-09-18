3f683d99853e6677d4f5fd92e65bd6cdee5eecf4
Prateek Pandey <orion.prateek@gmail.com>
2017-09-18 14:14:47 +0530

    Upload Files

---
+++ index.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const orderDb = [
+  {
+    orderId: 'OR100001',
+    productList: [
+      {
+        productId: 'PR100001'
+        quantity: '2'
+      }
+    ],
+    orderPlacementDate: 'June 23, 2017',
+    value: '20 £',
+    status: 'closed'
+  },
+  {
+    orderId: 'OR100002',
+    productList: [
+      {
+        productId: 'PR100001'
+        quantity: '2'
+      },
+      {
+        productId: 'PR100004'
+        quantity: '1'
+      }
+    ],
+    orderPlacementDate: 'July 2, 2017',
+    value: '35 £',
+    status: 'closed'
+  },
+  {
+    orderId: 'OR100003',
+    productList: [
+      {
+        productId: 'PR100002'
+        quantity: '3'
+      }
+    ],
+    orderPlacementDate: 'August 15, 2017',
+    value: '15 £',
+    status: 'closed'
+  },
+  {
+    orderId: 'OR100004',
+    productList: [
+      {
+        productId: 'PR100001'
+        quantity: '4'
+      }
+    ],
+    orderPlacementDate: 'September 2, 2017',
+    value: '40 £',
+    status: 'closed'
+  },
+  {
+    orderId: 'OR100005',
+    productList: [
+      {
+        productId: 'PR100001'
+        quantity: '2'
+      },
+      {
+        productId: 'PR100002'
+        quantity: '3'
+      },
+      {
+        productId: 'PR100003'
+        quantity: '5'
+      }
+    ],
+    orderPlacementDate: 'September 12, 2017',
+    value: '90 £',
+    status: 'closed'
+  }
+]
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post('/enquireOrder', function(req, res) {
+    var speech = 'Checking Req';
+
+    var reqText = req.query
+
+    return res.json({
+        speech: req.query,
+        displayText: speech,
+        source: 'webhook-orderApi-sample'
+    });
+});
+
+app.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
