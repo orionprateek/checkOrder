1b725fdbd13af545105c14f95d7a3c2c17e9612e
Prateek Pandey <orion.prateek@gmail.com>
2017-09-22 16:25:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,9 +108,9 @@ app.use(bodyParser.json());
 
 app.post('/enquireOrder', function(req, res) {
     console.log('This is request', req.body.originalRequest.data)
-    var accessToken = req.body.originalRequest.data.user.access_token ? req.body.originalRequest.data.user.access_token : 'noToken'
-    console.log('This: ', req.body.originalRequest.data.inputs[0].rawInputs)
-    console.log('This: ', req.body.originalRequest.data.inputs[0].arguments)
+//     var accessToken = req.body.originalRequest.data.user.access_token ? req.body.originalRequest.data.user.access_token : 'noToken'
+//     console.log('This: ', req.body.originalRequest.data.inputs[0].rawInputs)
+//     console.log('This: ', req.body.originalRequest.data.inputs[0].arguments)
     var speech
       , openCounter = 0
       , intent = req.body.result && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : "noIntent";
